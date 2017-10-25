a52b2ab98f152123b615ef6b43d77ad027accfd3
Shang-Yun Wu <maggiewu@24-1-107.dynamic.csail.mit.edu>
2017-10-25 12:48:16 -0400

    Initial Commit

---
+++ Team_Dave_Remote_Control_App_v2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Team_Dave_Remote_Control_App_v2.0
+//
+//  Created by Shang-Yun Wu on 10/25/17.
+//  Copyright © 2017 Shang-Yun Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
